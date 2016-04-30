363c676d2f6f0e4df89dc9dbe7b3d17ccdcbbcfb
Lucien Boudy <luboudy@gmail.com>
2016-04-30 11:18:43 +0200

    first commit

---
+++ Assets/Scripts/RawDataModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class RawDataModel : MonoBehaviour
+{
+	public Tango.TangoUnityImageData[] videoImages;
+    public Vector3[][] depthPoints;
+
+	/*
+	public static void SaveData (Tango.TangoUnityImageData[] videoImages, Vector3[][] depthPoints)
+	{
+		RawDataModel rawData = new RawDataModel();
+
+		rawData.videoImages = videoImages;
+		rawData.depthPoints = depthPoints;
+
+		string json = JsonUtility.ToJson(rawData);
+	}
+	*/
+}
---
+++ Assets/Scripts/SaveRawData.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+using Tango;
+
+public class SaveRawData : MonoBehaviour, ITangoVideoOverlay, ITangoDepth
+{
+	public TangoPointCloud tangoPointCloud;
+
+	private List<Tango.TangoUnityImageData> videoImages;
+	private List<Vector3[]> depthPoints;
+
+
+	public void StopRecord ()
+	{
+		Save();
+		Reset();
+	}
+
+	public void OnTangoImageAvailableEventHandler (Tango.TangoEnums.TangoCameraId cameraId, Tango.TangoUnityImageData imageBuffer)
+	{
+		videoImages.Add(imageBuffer);	
+	}
+
+    public void OnTangoDepthAvailable (TangoUnityDepth tangoDepth)
+	{
+		if (tangoPointCloud || tangoDepth == null || tangoDepth.m_points == null)
+			return;
+
+		depthPoints.Add(tangoPointCloud.m_points);
+	}
+
+	private void Start ()
+	{
+		Reset();
+	}
+
+	private void Reset ()
+	{
+		videoImages = new List<Tango.TangoUnityImageData>();
+		depthPoints = new List<Vector3[]>();
+	}
+
+	private void Save ()
+	{
+		RawDataModel rawData = new RawDataModel();
+
+		rawData.videoImages = videoImages.ToArray();
+		rawData.depthPoints = depthPoints.ToArray();
+
+		string json = JsonUtility.ToJson(rawData);
+	}
+}
