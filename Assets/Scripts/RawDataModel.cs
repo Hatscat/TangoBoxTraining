6338db7e06a78fbc5afe42af939b4f12e9208030
Lucien Boudy <luboudy@gmail.com>
2016-04-30 15:36:10 +0200

    save data

--- Assets/Scripts/RawDataModel.cs
+++ Assets/Scripts/RawDataModel.cs
@@ -1,21 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public class Vec3
+{
+	public float x, y, z;
+
+	public Vec3 (float _x, float _y, float _z)
+	{
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+}
+
+[Serializable]
+public class FrameData
+{
+	public double timestamp;
+	public Vec3[] depthPoints;
+	public byte[] colorPixels;
+	public uint imageWidth;
+
+	public FrameData (double _timestamp, Vector3[] _depthPoints, Byte[] _colorPixels, uint _imageWidth)
+	{
+		timestamp = _timestamp;
+		colorPixels = _colorPixels;
+		imageWidth = _imageWidth;
+		depthPoints = new Vec3[_depthPoints.Length];
+
+		for (int i = 0; i < _depthPoints.Length; ++i)
+		{
+			depthPoints[i] = new Vec3(_depthPoints[i].x, _depthPoints[i].y, _depthPoints[i].z);		
+		}
+	}
+}
 
 [Serializable]
 public class RawDataModel : MonoBehaviour
 {
-	public Tango.TangoUnityImageData[] videoImages;
-    public Vector3[][] depthPoints;
+	public FrameData[] framesData;
 
-	/*
-	public static void SaveData (Tango.TangoUnityImageData[] videoImages, Vector3[][] depthPoints)
+	public RawDataModel(List<FrameData> _framesData)
 	{
-		RawDataModel rawData = new RawDataModel();
-
-		rawData.videoImages = videoImages;
-		rawData.depthPoints = depthPoints;
-
-		string json = JsonUtility.ToJson(rawData);
+		framesData = _framesData.ToArray();
 	}
-	*/
 }
--- Assets/Scripts/SaveRawData.cs
+++ Assets/Scripts/SaveRawData.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System;
 using Tango;
 
-public class SaveRawData : MonoBehaviour, ITangoVideoOverlay, ITangoDepth
+public class SaveRawData : MonoBehaviour, ITangoVideoOverlay
 {
 	public TangoPointCloud tangoPointCloud;
+	[Range(0.01f, 2f)]
+	public float frameTimeIntervale = 0.2f; // sec
+	[Range(0.1f, 40f)]
+	public float timerMax = 15f; // sec
 
-	private List<Tango.TangoUnityImageData> videoImages;
-	private List<Vector3[]> depthPoints;
+	private Tango.TangoUnityImageData lastVideoImages;
+	private List<FrameData> framesData;
 	private bool record;
+	private float timer;
+	private string json = null;
+	private string error = null;
 
 	public void StopRecord ()
 	{
@@ -19,49 +28,67 @@ public class SaveRawData : MonoBehaviour, ITangoVideoOverlay, ITangoDepth
 
 	public void OnTangoImageAvailableEventHandler (Tango.TangoEnums.TangoCameraId cameraId, Tango.TangoUnityImageData imageBuffer)
 	{
-		if (!record)
-			return;
-		
         if (imageBuffer != null)
-            videoImages.Add(imageBuffer);	
-	}
-
-    public void OnTangoDepthAvailable (TangoUnityDepth tangoDepth)
-	{
-		if (!record || tangoPointCloud || tangoDepth == null || tangoDepth.m_points == null)
-			return;
-
-		depthPoints.Add(tangoPointCloud.m_points);
+            lastVideoImages = imageBuffer;	
 	}
 
 	private void Awake ()
 	{
-		DontDestroyOnLoad(gameObject);
-		//Reset();
+		//DontDestroyOnLoad(gameObject);
+		Reset();
 	}
 
 	private void OnGUI ()
 	{
-		if (GUI.Button (new Rect (Screen.width * 0.1f, Screen.height * 0.8f, 150, 100), record ? "Stop" : "Record"))
+		if (GUI.Button (new Rect(Screen.width * 0.1f, Screen.height * 0.8f, 150, 100), record ? "Stop" : "Record"))
 		{
 			record = !record;
+
+			if (record)
+				Capture();
+			else
+				Save();
 		}
+
+		if (json != null)
+			GUI.Label(new Rect(Screen.width * 0.1f, Screen.height * 0.1f, Screen.width * 0.1f, Screen.height * 0.1f), json);
+		else if (error != null)
+			GUI.Label(new Rect(Screen.width * 0.1f, Screen.height * 0.1f, Screen.width * 0.1f, Screen.height * 0.1f), error);
 	}
 
 	private void Reset ()
 	{
-		videoImages = new List<Tango.TangoUnityImageData>();
-		depthPoints = new List<Vector3[]>();
+		lastVideoImages = null;
+		framesData = new List<FrameData>();
+		record = false;
+		timer = 0f;
 	}
 
 	private void Save ()
 	{
-		RawDataModel rawData = new RawDataModel();
+		RawDataModel rawData = new RawDataModel(framesData);
 
-		rawData.videoImages = videoImages.ToArray();
-		rawData.depthPoints = depthPoints.ToArray();
+		File.WriteAllText(Application.persistentDataPath + "/tessst2.json", "{'test': 2}");
 
-		string json = JsonUtility.ToJson(rawData);
-        File.WriteAllText(Application.dataPath + "./rawData.json", json);
+		try
+		{
+			json = JsonUtility.ToJson(rawData);
+		}
+		catch (Exception e)
+		{
+			error = e.ToString();
+		}
+
+        File.WriteAllText(Application.persistentDataPath + "/rawData.json", json);
+	}
+
+	private IEnumerator Capture ()
+	{
+		while (record)
+		{
+			if (framesData != null && tangoPointCloud != null && tangoPointCloud.m_points != null && lastVideoImages != null && lastVideoImages.data != null)
+				framesData.Add(new FrameData(timer, tangoPointCloud.m_points, lastVideoImages.data, lastVideoImages.width));
+			yield return new WaitForSeconds(frameTimeIntervale);
+		}
 	}
 }
